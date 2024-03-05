525539b767284f6d1bc7c6b212f351800f2be814
karina <karinaslusar246@gmail.com>
2024-03-05 19:33:53 +0200

    practical 3

--- Pract/MyForm.h
+++ Pract/MyForm.h
@@ -34,12 +34,17 @@ namespace Pract {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::RichTextBox^ richTextBox1;
+	protected:
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
 
 	private:
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+		System::ComponentModel::Container^ components;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -48,12 +53,94 @@ namespace Pract {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// richTextBox1
+			// 
+			this->richTextBox1->Location = System::Drawing::Point(12, 12);
+			this->richTextBox1->Name = L"richTextBox1";
+			this->richTextBox1->Size = System::Drawing::Size(443, 221);
+			this->richTextBox1->TabIndex = 0;
+			this->richTextBox1->Text = L"";
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(45, 263);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(105, 23);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"Згенерувати";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(333, 263);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->TabIndex = 2;
+			this->button2->Text = L"Закрити";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(189, 263);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(105, 23);
+			this->button3->TabIndex = 3;
+			this->button3->Text = L"Знайти кількість";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(466, 312);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->richTextBox1);
+			this->Name = L"MyForm";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
+			this->Text = L"Application";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+		Application::Exit();
+	}
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		richTextBox1->Clear();
+		Random^ rand = gcnew Random();
+		for (int i = 0; i < 12; i++) {
+			richTextBox1->Text += rand->Next(-100, 100).ToString() + "\n";
+		}
+	}
+	private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
+		// find count of negative and positive numbers
+		int countPositive = 0;
+		int countNegative = 0;
+		array<String^>^ lines = richTextBox1->Text->Split('\n');
+		for (int i = 0; i < lines->Length; i++) {
+			if (lines[i] != "") {
+				int number = Convert::ToInt32(lines[i]);
+				if (number > 0) {
+					countPositive++;
+				}
+				else if (number < 0) {
+					countNegative++;
+				}
+			}
+		}
+		MessageBox::Show("Кількість додатніх чисел: " + countPositive + "\nКількість від'ємних чисел: " + countNegative);
+
+	}
 	};
 }
